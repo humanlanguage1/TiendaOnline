6ced2b0ec77219199af0868339c45a4ec6892515
Diego Linares <diegoalonsoll@hotmail.com>
2022-01-07 15:17:21 -0500

    añadido css

--- demoStore/tienda/models.py
+++ demoStore/tienda/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Categoria(models.Model):
@@ -17,3 +18,12 @@ class Producto(models.Model):
     
     def __str__(self):
             return self.nombre
+
+class Cliente(models.Model):
+      user = models.OneToOneField(User,on_delete=models.RESTRICT)
+      doc_ide= models.CharField(max_length=200,blank=True)
+      direccion = models.CharField(max_length=200,blank=True)
+      telefono = models.CharField(max_length=20,blank=True)
+
+      def __str__(self):
+            return self.usuario.username
